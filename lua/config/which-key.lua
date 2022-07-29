8b56f096c43200dde22830704722618015c20cb2
rainmodred <albphoenix@gmail.com>
2022-07-29 23:48:36 +0300

    Fix which-key mappings

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -29,15 +29,6 @@ local mappings = {
   -- ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
   -- ["P"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
 
-  p = {
-    name = "Packer",
-    c = { "<cmd>PackerCompile<cr>", "Compile" },
-    i = { "<cmd>PackerInstall<cr>", "Install" },
-    s = { "<cmd>PackerSync<cr>", "Sync" },
-    S = { "<cmd>PackerStatus<cr>", "Status" },
-    u = { "<cmd>PackerUpdate<cr>", "Update" },
-  },
-
   -- g = {
   --   name = "Git",
   --   g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
