c64674de25ab9e6ed39fd4605b82c0d8ceb45144
rainmodred <albphoenix@gmail.com>
2022-07-31 01:58:52 +0300

    Add css lsp servers

--- lua/config/lsp/configs.lua
+++ lua/config/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua", "tsserver", "eslint" }
+local servers = { "jsonls", "sumneko_lua", "tsserver", "eslint", "cssls", "cssmodules_ls" }
 
 lsp_installer.setup({
   ensure_installed = servers,
