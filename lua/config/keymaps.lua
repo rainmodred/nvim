c2266d97b6f6fc8ddd16c11d63a5ad6bae58c9db
rainmodred <albphoenix@gmail.com>
2022-08-29 11:24:34 +0300

    Add close buffer keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -45,6 +45,33 @@ keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 -- Tab switch buffer
 keymap("n", "<S-l>", ":BufferLineCycleNext<CR>", opts)
 keymap("n", "<S-h>", ":BufferLineCyclePrev<CR>", opts)
+
+-- Close buffer
+function DeleteCurrentBuffer()
+  local cbn = vim.api.nvim_get_current_buf()
+  local buffers = vim.fn.getbufinfo({ buflisted = true })
+  local size = 0
+  local idx = 0
+  for n, e in ipairs(buffers) do
+    size = size + 1
+    if e.bufnr == cbn then
+      idx = n
+    end
+  end
+  if idx == 0 then
+    -- return
+    vim.cmd("NvimTreeFocus")
+  end
+  if idx == size then
+    vim.cmd("bprevious")
+  else
+    vim.cmd("bnext")
+  end
+  vim.cmd("bdelete " .. cbn)
+end
+
+keymap("n", "<leader>d", "<cmd>lua DeleteCurrentBuffer()<cr>", opts)
+
 -- Close Tab
 keymap("n", "<S-q>", "<cmd>bdelete<CR>", opts)
 
