c8f4f77f375ee5927b763cf4ac0ba3a2c857911d
rainmodred <albphoenix@gmail.com>
2023-02-12 14:38:17 +0300

    Fix nvim tree

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -4,6 +4,10 @@ if not status_ok then
 end
 
 nvim_tree.setup({
+  update_focused_file = {
+    enable = true,
+    update_cwd = false,
+  },
   sort_by = "case_sensitive",
   view = {
     width = 30,
