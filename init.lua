613f4722f80258b6b1a99bb405331fbb9bc8b3aa
rainmodred <albphoenix@gmail.com>
2023-02-19 15:28:57 +0300

    Add highligh on yank

--- init.lua
+++ init.lua
@@ -47,3 +47,14 @@ end
 vim.api.nvim_command([[
     autocmd ModeChanged * lua leave_snippet()
 ]])
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
+})
