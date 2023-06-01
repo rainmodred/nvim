9b4667e261d5f61305001ee01a8801c7f6a4239d
rainmodred <albphoenix@gmail.com>
2023-06-01 15:52:08 +0300

    Update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,12 @@ vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Open floating dia
 
 -- add undo break point
 vim.keymap.set("i", "{", "{<c-g>u")
+vim.keymap.set("i", "(", "(<c-g>u")
+vim.keymap.set("i", " ", " <c-g>u")
+vim.keymap.set("i", "$", "$<c-g>u")
+
+-- Replace words
+vim.keymap.set("n", "<leader>se", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Rename word" })
+
+-- Paste with deleting selected text
+vim.keymap.set("x", "<leader>p", [["_dP]])
