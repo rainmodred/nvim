d632e1508b1119bf06efcb516cdc601143c28095
rainmodred <albphoenix@gmail.com>
2025-01-28 16:08:12 +0300

    Disable themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,11 @@
 return {
-
-  { "ellisonleao/gruvbox.nvim" },
-  { "rose-pine/neovim", name = "rose-pine" },
-  { "nyoom-engineering/oxocarbon.nvim" },
-  {
-    "projekt0n/github-nvim-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-  },
+  -- { "ellisonleao/gruvbox.nvim" },
+  -- { "nyoom-engineering/oxocarbon.nvim" },
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  -- },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
