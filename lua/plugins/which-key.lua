5ac619cd57700fd79d42f00a0283f97a1d2aecf6
rainmodred <albphoenix@gmail.com>
2025-08-22 23:03:18 +0300

    Change which-key delay

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,7 +2,7 @@ return { -- Useful plugin to show you pending keybinds.
   "folke/which-key.nvim",
   event = "VeryLazy", -- Sets the loading event to 'VimEnter'
   opts = {
-    delay = 500,
+    delay = 200,
     preset = "helix",
     spec = {
       { "<leader>s", group = "[S]earch" },
