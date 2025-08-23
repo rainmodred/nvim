2d8a1df0dd54373e8381fc3172f9b75e6db62816
rainmodred <albphoenix@gmail.com>
2025-08-23 15:21:47 +0300

    Disable virtual_text

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -91,54 +91,19 @@ return {
           [vim.diagnostic.severity.HINT] = "󰌶 ",
         },
       } or {},
-      virtual_text = {
-        source = "if_many",
-        spacing = 2,
-        format = function(diagnostic)
-          local diagnostic_message = {
-            [vim.diagnostic.severity.ERROR] = diagnostic.message,
-            [vim.diagnostic.severity.WARN] = diagnostic.message,
-            [vim.diagnostic.severity.INFO] = diagnostic.message,
-            [vim.diagnostic.severity.HINT] = diagnostic.message,
-          }
-          return diagnostic_message[diagnostic.severity]
-        end,
-      },
+      virtual_text = false,
     })
-
-    -- LSP servers and clients are able to communicate to each other what features they support.
-    --  By default, Neovim doesn't support everything that is in the LSP specification.
-    --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
-    --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
     local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-    -- Enable the following language servers
-    --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --
-    --  Add any additional override configuration in the following tables. Available keys are:
-    --  - cmd (table): Override the default command used to start the server
-    --  - filetypes (table): Override the default list of associated filetypes for the server
-    --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    --  - settings (table): Override the default settings passed when initializing the server.
-    --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      -- clangd = {},
       gopls = {},
-      -- pyright = {},
       -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      ts_ls = {},
-      --
+      ts_ls = {
+        capabilities = {},
+      },
 
       lua_ls = {
-        -- cmd = { ... },
         -- filetypes = { ... },
-        -- capabilities = {},
+        --capabilities = {},
         settings = {
           Lua = {
             completion = {
@@ -147,26 +112,15 @@ return {
           },
         },
       },
+      volar = {},
     }
 
-    -- Ensure the servers and tools above are installed
-    --
-    -- To check the current status of installed tools and/or manually install
-    -- other tools, you can run
-    --    :Mason
-    --
-    -- You can press `g?` for help in this menu.
-    --
-    -- `mason` had to be setup earlier: to configure its options see the
-    -- `dependencies` table for `nvim-lspconfig` above.
-    --
-    -- You can add other tools here that you want Mason to install
-    -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       "stylua",
       "biome",
       "prettierd",
+      "prettier",
     })
     require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
@@ -178,8 +132,12 @@ return {
           local server = servers[server_name] or {}
 
           if server_name == "ts_ls" then
-            --TODO:
             print(server_name)
+
+            -- server.capabilities = false
+            -- server.capabilities.textDocument.formatting = false
+            -- server.capabilities.textDocument.rangeFormatting = false
+            -- TODO:
           end
 
           -- This handles overriding only values explicitly passed
