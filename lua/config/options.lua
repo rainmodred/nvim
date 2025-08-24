c1c8fa64290e25aa381eca5f0fd194d32cc3f97d
rainmodred <albphoenix@gmail.com>
2025-08-24 17:06:28 +0300

    Disable netrw

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
 
 vim.o.number = true
 vim.o.relativenumber = true
