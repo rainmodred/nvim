2aa20d89c078450381a8302e056ddb13cdcfee4c
rainmodred <albphoenix@gmail.com>
2025-08-26 19:18:30 +0300

    Add transparent background

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -8,6 +8,10 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
+
       vim.cmd.colorscheme("catppuccin")
     end,
   },
