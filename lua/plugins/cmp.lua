ad41aa69fe4fddf23e8e38c9240aa009c9471df5
rainmodred <albphoenix@gmail.com>
2025-08-28 22:45:45 +0300

    Fix snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -17,18 +17,10 @@ return {
           end
           return "make install_jsregexp"
         end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-        opts = {},
+        dependencies = {},
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets" })
+        end,
       },
       "folke/lazydev.nvim",
     },
